532d226da205be6c335d8a0f4461e88bf994eada
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2010-01-03 01:41:39 +0000

    3.6.0.1561 stable: x 修正"連結開新頁", "外鏈連結開新頁"及"轉換視頻網站連結為影片"在FF下對連結使用中鍵或右鍵同樣起到作用的bug

--- other/an.v3.main.js
+++ other/an.v3.main.js
@@ -4,13 +4,13 @@ AN.box.external.main =
 	{
 		"stable":
 		{
-			"annuus": "3.5.7.908",
+			"annuus": "3.6.0.1561",
 			"Kernel": "3.5.0"
 		},
 
 		"beta":
 		{
-			"annuus": "3.6.0.1559",
+			"annuus": "3.6.0.1561",
 			"Kernel": "3.5.0"
 		}
 	}
