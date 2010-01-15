7a85c5300b0906f0fdc9a07aba23990401f96967
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2010-01-15 19:26:45 +0000

    3.7.0.2778 stable:
    * 新增"限制回覆高度"
    + 更新jQuery為1.4
    + 改進內核以優化"標題過濾功能", "屏蔽圖片", "用戶屏蔽功能", "用戶高亮功能", "用戶單獨顯示功能"及"改變引用風格"
    + 重寫以優化"強化表情圖示列"
    + 改進"自動檢查更新", 部份流覽器現在在發現更新時會自動打開相關檔案
    + 改進"強制鎖定闊度"
    + 優化"轉換文字連結成連結", "轉換圖片連結成圖片"及"轉換視頻網站連結成影片"
    x 修正"Ajax化頁面讀取"於部份情況下發送回覆會導致重新整理的bug
    x 修正"自動檢查更新"在Maxthon下自動打開檔案的位置出錯的bug
    - 快速回覆的預覽現在不會受其他功能影響

--- other/an.v3.main.js
+++ other/an.v3.main.js
@@ -4,13 +4,13 @@ AN.box.external.main =
 	{
 		"stable":
 		{
-			"annuus": "3.6.6.2428",
+			"annuus": "3.7.0.2778",
 			"Kernel": "3.5.0"
 		},
 
 		"beta":
 		{
-			"annuus": "3.6.6.2428",
+			"annuus": "3.7.0.2778",
 			"Kernel": "3.5.0"
 		}
 	}
