4969f99c8648c5b696e7419d919c97f038eb253a
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2010-01-29 08:55:16 +0000

    3.7.4.3942 stable:
    x 修正Chrome擴展無效的bug

--- other/an.v3.main.js
+++ other/an.v3.main.js
@@ -4,13 +4,13 @@ AN.box.external.main =
 	{
 		"stable":
 		{
-			"annuus": "3.7.3.3940",
+			"annuus": "3.7.4.3942",
 			"Kernel": "3.5.0"
 		},
 
 		"beta":
 		{
-			"annuus": "3.7.3.3940",
+			"annuus": "3.7.4.3942",
 			"Kernel": "3.5.0"
 		}
 	}
