d7a3e31856143a1172690be803e6d03fdbb389e3
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2010-02-01 03:44:28 +0000

    3.7.4.3990 stable:
    + 改進部份功能顯示圖示的設定
    x 修正"Ajax化頁面讀取"自動重試無效的bug

--- other/an.v3.main.js
+++ other/an.v3.main.js
@@ -4,13 +4,13 @@ AN.box.external.main =
 	{
 		"stable":
 		{
-			"annuus": "3.7.4.3942",
+			"annuus": "3.7.5.3990",
 			"Kernel": "3.5.0"
 		},
 
 		"beta":
 		{
-			"annuus": "3.7.4.3942",
+			"annuus": "3.7.5.3990",
 			"Kernel": "3.5.0"
 		}
 	}
