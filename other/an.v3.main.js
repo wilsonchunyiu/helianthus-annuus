5e42f950474b5098b3ec74079da2a3e996979742
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2010-02-05 08:15:42 +0000

    3.7.7.4363 stable:
    x 修正FF擴展狀態欄圖示沒有自動隱藏的bug
    x 修正"Ajax化頁面讀取"於延展模式發現新頁時沒有即時讀取的bug

--- other/an.v3.main.js
+++ other/an.v3.main.js
@@ -4,13 +4,13 @@ AN.box.external.main =
 	{
 		"stable":
 		{
-			"annuus": "3.7.6.4063",
+			"annuus": "3.7.7.4363",
 			"Kernel": "3.5.0"
 		},
 
 		"beta":
 		{
-			"annuus": "3.7.7.4262",
+			"annuus": "3.7.7.4363",
 			"Kernel": "3.5.0"
 		}
 	}
