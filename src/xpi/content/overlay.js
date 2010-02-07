63a552fcb7d3c0bc439938452ded4eb25ba69aa1
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2010-02-07 18:36:22 +0000

    move tags, moved to wrong dir last time

--- src/xpi/content/overlay.js
+++ src/xpi/content/overlay.js
@@ -1,51 +1,44 @@
 window.addEventListener('load', function()
 {
-	var
-	pref = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch).getBranch('extensions.annuus.'),
-	status = pref.getBoolPref('status'),
-	overlay = document.getElementById('annuus-overlay'),
-	rHKG = /^http:\/\/forum\d+\.hkgolden\.com/i,
-	noop = function(){};
+	var pref = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch).getBranch('extensions.annuus.');
+	var status = pref.getBoolPref('switchedOn');
+	var overlay = document.getElementById('annuus-overlay');
+	var rHKGolden = /^http:\/\/forum\d+\.hkgolden\.com/i;
 
 	function setStatus()
 	{
-		overlay.src = status ? 'chrome://annuus/skin/status-on.png' : 'chrome://annuus/skin/status-off.png';
+		overlay.setAttribute('status', status ? 'on' : 'off');
 	}
+
 	setStatus();
 
-	overlay.addEventListener('command', function()
+	overlay.addEventListener('mouseup', function()
 	{
 		status = !status;
 		setStatus();
-		pref.setBoolPref('status', status);
+		pref.setBoolPref('switchedOn', status);
+	}, false);
+
+	document.getElementById('appcontent').addEventListener('DOMContentLoaded', function(event)
+	{
+		var doc = event.originalTarget;
+		if(status && doc.nodeName == '#document' && rHKGolden.test(doc.location.href))
+		{
+			var script = doc.createElement('script');
+			script.charset = 'utf-8';
+			script.src = 'resource://annuus/annuus.js';
+			doc.getElementsByTagName('head')[0].appendChild(script);
+		}
 	}, false);
 
 	gBrowser.addProgressListener({
 		onLocationChange: function(progress, request, uri)
 		{
-			overlay.hidden = !(uri && rHKG.test(uri.prePath));
+			overlay.setAttribute('hidden', !(uri && rHKGolden.test(uri.prePath)));
 		},
-		onStateChange: noop,
-		onProgressChange: noop,
-		onStatusChange: noop,
-		onSecurityChange: noop
-	});
-
-	gBrowser.addTabsProgressListener({
-		onLocationChange: function(browser, progress, request, uri)
-		{
-			if(progress.isLoadingDocument && status && rHKG.test(uri.prePath)) {
-				var
-				doc = browser.contentDocument,
-				script = doc.createElement('script');
-				script.charset = 'utf-8';
-				script.src = 'resource://annuus/annuus.js';
-				doc.getElementsByTagName('head')[0].appendChild(script);
-			}
-		},
-		onStateChange: noop,
-		onProgressChange: noop,
-		onStatusChange: noop,
-		onSecurityChange: noop
-	});
+		onStateChange: function(){},
+		onProgressChange: function(){},
+		onStatusChange: function(){},
+		onSecurityChange: function(){}
+	}, Components.interfaces.nsIWebProgress.NOTIFY_LOCATION);
 }, false);
