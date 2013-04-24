90dec9f8f99158ac3cc2081900a9a0acce840be9
project.helianthus <project.helianthus@28ed74e2-b0a3-11dd-8075-91d349ce83ee>
2013-04-24 17:10:32 +0000

    dist stable 3.10.0.5686

--- other/an.v3.main.js
+++ other/an.v3.main.js
@@ -4,13 +4,13 @@ AN.box.external.main =
 	{
 		"stable":
 		{
-			"annuus": "3.9.10.5604",
+			"annuus": "3.10.0.5686",
 			"Kernel": "3.5.0"
 		},
 
 		"beta":
 		{
-			"annuus": "3.9.11.5671",
+			"annuus": "3.10.0.5686",
 			"Kernel": "3.5.0"
 		}
 	}
